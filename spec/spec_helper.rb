72d2b27e45678a86963937089c142b33f23dbad3
Keith Thornhill <kthornhill@red5studios.com>
2013-04-23 16:32:30 -0700

    add rcov output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require 'rspec'
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.add_filter 'vendor'
+SimpleCov.add_filter 'spec'
+SimpleCov.start
+
 require_relative '../lib/hystrix.rb'
 
 RSpec.configure do |config|
